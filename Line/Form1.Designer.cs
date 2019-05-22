7070e19f00437153cbf50ccf53a517e497e34772
ztshat <ztshat@gmail.com>
2019-05-22 14:31:41 +0300

    Створення репозиторію

---
+++ Line/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Line
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pb_Result_A = new System.Windows.Forms.PictureBox();
+            this.btn_Create = new System.Windows.Forms.Button();
+            this.btn_Generate = new System.Windows.Forms.Button();
+            this.pb_Result_B = new System.Windows.Forms.PictureBox();
+            this.nud_Max = new System.Windows.Forms.NumericUpDown();
+            this.nud_Min = new System.Windows.Forms.NumericUpDown();
+            this.nud_A = new System.Windows.Forms.NumericUpDown();
+            this.nud_B = new System.Windows.Forms.NumericUpDown();
+            this.dgv_PayMatrix = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Result_A)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Result_B)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_Max)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_Min)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_A)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_B)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_PayMatrix)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(275, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pb_Result_A
+            // 
+            this.pb_Result_A.BackColor = System.Drawing.Color.White;
+            this.pb_Result_A.Location = new System.Drawing.Point(374, 12);
+            this.pb_Result_A.Name = "pb_Result_A";
+            this.pb_Result_A.Size = new System.Drawing.Size(500, 500);
+            this.pb_Result_A.TabIndex = 5;
+            this.pb_Result_A.TabStop = false;
+            this.pb_Result_A.Paint += new System.Windows.Forms.PaintEventHandler(this.pb_Result_Paint);
+            // 
+            // btn_Create
+            // 
+            this.btn_Create.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Create.Location = new System.Drawing.Point(8, 292);
+            this.btn_Create.Name = "btn_Create";
+            this.btn_Create.Size = new System.Drawing.Size(89, 30);
+            this.btn_Create.TabIndex = 0;
+            this.btn_Create.Text = "Create";
+            this.btn_Create.UseVisualStyleBackColor = true;
+            this.btn_Create.Click += new System.EventHandler(this.btn_Create_Click);
+            // 
+            // btn_Generate
+            // 
+            this.btn_Generate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Generate.Location = new System.Drawing.Point(136, 292);
+            this.btn_Generate.Name = "btn_Generate";
+            this.btn_Generate.Size = new System.Drawing.Size(89, 30);
+            this.btn_Generate.TabIndex = 0;
+            this.btn_Generate.Text = "Generate";
+            this.btn_Generate.UseVisualStyleBackColor = true;
+            this.btn_Generate.Click += new System.EventHandler(this.btn_Generate_Click);
+            // 
+            // pb_Result_B
+            // 
+            this.pb_Result_B.BackColor = System.Drawing.Color.White;
+            this.pb_Result_B.Location = new System.Drawing.Point(880, 12);
+            this.pb_Result_B.Name = "pb_Result_B";
+            this.pb_Result_B.Size = new System.Drawing.Size(500, 500);
+            this.pb_Result_B.TabIndex = 5;
+            this.pb_Result_B.TabStop = false;
+            this.pb_Result_B.Paint += new System.Windows.Forms.PaintEventHandler(this.pb_Result_Paint);
+            // 
+            // nud_Max
+            // 
+            this.nud_Max.Location = new System.Drawing.Point(244, 12);
+            this.nud_Max.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.nud_Max.Name = "nud_Max";
+            this.nud_Max.Size = new System.Drawing.Size(120, 20);
+            this.nud_Max.TabIndex = 7;
+            this.nud_Max.Value = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            // 
+            // nud_Min
+            // 
+            this.nud_Min.Location = new System.Drawing.Point(244, 38);
+            this.nud_Min.Maximum = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.nud_Min.Minimum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            -2147483648});
+            this.nud_Min.Name = "nud_Min";
+            this.nud_Min.Size = new System.Drawing.Size(120, 20);
+            this.nud_Min.TabIndex = 8;
+            this.nud_Min.Value = new decimal(new int[] {
+            9,
+            0,
+            0,
+            -2147483648});
+            // 
+            // nud_A
+            // 
+            this.nud_A.Location = new System.Drawing.Point(56, 12);
+            this.nud_A.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nud_A.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud_A.Name = "nud_A";
+            this.nud_A.Size = new System.Drawing.Size(120, 20);
+            this.nud_A.TabIndex = 7;
+            this.nud_A.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // nud_B
+            // 
+            this.nud_B.Location = new System.Drawing.Point(56, 38);
+            this.nud_B.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nud_B.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud_B.Name = "nud_B";
+            this.nud_B.Size = new System.Drawing.Size(120, 20);
+            this.nud_B.TabIndex = 7;
+            this.nud_B.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // dgv_PayMatrix
+            // 
+            this.dgv_PayMatrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_PayMatrix.Location = new System.Drawing.Point(8, 64);
+            this.dgv_PayMatrix.Name = "dgv_PayMatrix";
+            this.dgv_PayMatrix.Size = new System.Drawing.Size(356, 222);
+            this.dgv_PayMatrix.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(27, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(27, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "B";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 412);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgv_PayMatrix);
+            this.Controls.Add(this.nud_Min);
+            this.Controls.Add(this.nud_B);
+            this.Controls.Add(this.nud_A);
+            this.Controls.Add(this.nud_Max);
+            this.Controls.Add(this.pb_Result_B);
+            this.Controls.Add(this.pb_Result_A);
+            this.Controls.Add(this.btn_Generate);
+            this.Controls.Add(this.btn_Create);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Graph";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Result_A)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Result_B)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_Max)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_Min)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_A)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_B)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_PayMatrix)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pb_Result_A;
+        private System.Windows.Forms.Button btn_Create;
+        private System.Windows.Forms.Button btn_Generate;
+        private System.Windows.Forms.PictureBox pb_Result_B;
+        private System.Windows.Forms.NumericUpDown nud_Max;
+        private System.Windows.Forms.NumericUpDown nud_Min;
+        private System.Windows.Forms.NumericUpDown nud_A;
+        private System.Windows.Forms.NumericUpDown nud_B;
+        private System.Windows.Forms.DataGridView dgv_PayMatrix;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Line/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Line
+{
+    public partial class Form1 : Form
+    {
+        Graphics graph_A;
+        Graphics graph_B;
+
+        Bitmap myBitmap_A;
+        Bitmap myBitmap_B;
+
+        Pen myBlackPen;
+        SolidBrush myRedBrush;
+        Color selectedColor;
+        int a;
+        int b;
+        int tranparent;
+        int lineThikness;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Підлаштовуємо розмір Зображення під форму
+            pb_Result_A.Width = 500;
+            pb_Result_A.Height = 500;
+
+
+            //створюємо віртуальне зображення: 
+            myBitmap_A = new Bitmap(500, 500);
+            myBitmap_B = new Bitmap(500, 500);
+            //підключаємо віртуальне зображення до графіки:
+            graph_A = Graphics.FromImage(myBitmap_A);
+            graph_B = Graphics.FromImage(myBitmap_B);
+            //створюємо об'єкт пензлик
+            lineThikness = 2;
+            myBlackPen = new Pen(Color.DimGray, lineThikness);
+            //початковий колір заливки
+            selectedColor = Color.Green;
+            //початкова прозорість
+            tranparent = 255;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //A
+            graph_A.Clear(Color.White);
+            //відображаємо зображення в компонент PictureBox:
+            pb_Result_A.Image = myBitmap_A;
+
+            //створюємо точки 
+            Point point_A = new Point(50, 450);
+            Point point_B = new Point(450, 450);
+            //малюємо вісь
+            graph_A.DrawLine(myBlackPen, point_A, point_B);
+            graph_A.DrawLine(myBlackPen, 440, 445, 451, 451);
+            graph_A.DrawLine(myBlackPen, 440, 455, 451, 449);
+            //лінії стратегій 
+            graph_A.DrawLine(myBlackPen, 75, 50, 75, 460);
+                //стрілка
+                graph_A.DrawLine(myBlackPen, 70, 60, 75, 50);
+                graph_A.DrawLine(myBlackPen, 80, 60, 75, 50);
+
+            graph_A.DrawLine(myBlackPen, 425, 50, 425, 460);
+                //стрілка
+                graph_A.DrawLine(myBlackPen, 430, 55, 425, 50);
+                graph_A.DrawLine(myBlackPen, 420, 55, 425, 50);
+            //
+            //підписи до осей http://foxtools.ru/Unicode 
+            graph_A.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            Font lFont = new Font("Microsoft Sans Serif", 16, FontStyle.Regular);
+           
+            graph_A.DrawString("A" + ((char)8321).ToString(), lFont, Brushes.Black, 40, 40);
+            graph_A.DrawString("A" + ((char)8322).ToString(), lFont, Brushes.Black, 430, 40);
+            graph_A.DrawString("p", lFont, Brushes.Black, 450, 460);
+            graph_A.DrawString("0", lFont, Brushes.Black, 65, 460);
+            graph_A.DrawString("1", lFont, Brushes.Black, 415, 460);
+            Font numFont = new Font("Microsoft Sans Serif", 8, FontStyle.Regular);
+            //підписи до осей A1
+            for (int i=1; i<=21; i++)
+            {
+                graph_A.DrawString((i-11).ToString(), numFont, Brushes.Gray, 40, 445-i*17);
+                graph_A.DrawLine(myBlackPen, 70, 450 - i * 17, 75, 450 - i * 17);
+
+            }
+            //підписи до осей A2
+            for (int i = 1; i <= 21; i++)
+            {
+                graph_A.DrawString((i - 11).ToString(), numFont, Brushes.Gray, 450, 445 - i * 17);
+                graph_A.DrawLine(myBlackPen, 425, 450 - i * 17, 430, 450 - i * 17);
+
+            }
+            //B
+            graph_B.Clear(Color.White);
+            //відображаємо зображення в компонент PictureBox:
+            pb_Result_B.Image = myBitmap_B;
+
+            //створюємо точки 
+
+            //малюємо вісь
+            graph_B.DrawLine(myBlackPen, point_A, point_B);
+            graph_B.DrawLine(myBlackPen, 440, 445, 451, 451);
+            graph_B.DrawLine(myBlackPen, 440, 455, 451, 449);
+            //лінії стратегій 
+            graph_B.DrawLine(myBlackPen, 75, 50, 75, 460);
+            //стрілка
+            graph_B.DrawLine(myBlackPen, 70, 60, 75, 50);
+            graph_B.DrawLine(myBlackPen, 80, 60, 75, 50);
+
+            graph_B.DrawLine(myBlackPen, 425, 50, 425, 460);
+            //стрілка
+            graph_B.DrawLine(myBlackPen, 430, 55, 425, 50);
+            graph_B.DrawLine(myBlackPen, 420, 55, 425, 50);
+            //
+            //підписи до осей http://foxtools.ru/Unicode 
+            graph_B.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+
+
+            graph_B.DrawString("B" + ((char)8321).ToString(), lFont, Brushes.Black, 40, 40);
+            graph_B.DrawString("B" + ((char)8322).ToString(), lFont, Brushes.Black, 430, 40);
+            graph_B.DrawString("q", lFont, Brushes.Black, 450, 460);
+            graph_B.DrawString("0", lFont, Brushes.Black, 65, 460);
+            graph_B.DrawString("1", lFont, Brushes.Black, 415, 460);
+            
+            //підписи до осей B1
+            for (int i = 1; i <= 21; i++)
+            {
+                graph_B.DrawString((i - 11).ToString(), numFont, Brushes.Gray, 40, 445 - i * 17);
+                graph_B.DrawLine(myBlackPen, 70, 450 - i * 17, 75, 450 - i * 17);
+
+            }
+            //підписи до осей B2
+            for (int i = 1; i <= 21; i++)
+            {
+                graph_B.DrawString((i - 11).ToString(), numFont, Brushes.Gray, 450, 445 - i * 17);
+                graph_B.DrawLine(myBlackPen, 425, 450 - i * 17, 430, 450 - i * 17);
+
+            }
+
+
+        }
+
+        private void pb_Result_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void btn_Create_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(nud_A.Value);
+            int b = Convert.ToInt32(nud_B.Value);
+            
+            dgv_PayMatrix.ColumnCount = b;
+            dgv_PayMatrix.RowCount = a;
+            dgv_PayMatrix.RowHeadersWidth = 70;
+            dgv_PayMatrix.ColumnHeadersVisible = true;
+
+            // Set the column header style.
+            DataGridViewCellStyle columnHeaderStyle = new DataGridViewCellStyle();
+
+            columnHeaderStyle.BackColor = Color.Beige;
+            columnHeaderStyle.Font = new Font("Verdana", 10, FontStyle.Regular);
+            columnHeaderStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgv_PayMatrix.ColumnHeadersDefaultCellStyle = columnHeaderStyle;
+
+
+            //dgw_PayMatrix.Columns.Add("Column", b.ToString());
+            for (int i = 0; i < b; i++)
+            {
+                // Set the column header names.
+                dgv_PayMatrix.Columns[i].Name = "B"+(i+1).ToString();
+                dgv_PayMatrix.Columns[i].Width = 50;
+            }
+
+            for (int i = 0; i < a; i++)
+            {
+                // Set the row header names.
+                dgv_PayMatrix.Rows[i].HeaderCell.Value = "A" + (i + 1).ToString();
+            }
+
+        }
+        //Заповнення випадковими даними http://www.cyberforum.ru/csharp-beginners/thread997446.html
+        private void btn_Generate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            for (int x = 0; x < dgv_PayMatrix.ColumnCount; x++)
+            {
+                for (int y = 0; y < dgv_PayMatrix.RowCount; y++)
+                {
+                    dgv_PayMatrix[x, y].Value = rnd.Next(Convert.ToInt32(nud_Min.Text), Convert.ToInt32(nud_Max.Text)); // Значения берутся рандомно из заданного пользователем интервала
+                }
+
+            }
+        }
+    }
+}
